6461c3a8701e6deb8081832fbc3bdbc4b0383210
Tyler Guice <guicetyler@aol.com>
2021-06-26 01:44:30 -0400

    Finishing up resume section, creating skills section, finessing testimonials, and starting contact section

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -15,19 +15,19 @@ export class TestimonialsComponent implements OnInit {
                'He has an aptitude for learning complex skills quickly, ' +
                'while providing some of the best user interface designs on the market.',
       author: 'David Wilt',
-      role: 'Application Manager, 4.5 years'
+      role: 'Application Manager'
     },
     {
       message: 'Tyler is loyal, dependable, is always seeking to improve, adheres to best practices, ' +
                'and has an excellent eye for detail. I\'ve worked with him for over four years ' +
                'and he has proven to be a valuable asset on many major projects.',
       author: 'Neil Purcell',
-      role: 'Lead Application Developer, 4.5 years'
+      role: 'Lead Application Developer'
     },
     {
       message: 'Placeholder text....',
       author: 'Anne-Marie Williams',
-      role: 'Lead Business Analyst, 4.5 years'
+      role: 'Lead Business Analyst'
     }
   ];
 
