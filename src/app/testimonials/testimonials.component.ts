4f68d2d3b1b06951edf6b6c929cb7da9bb50dbdc
Tyler Guice <guicetyler@aol.com>
2021-06-26 22:33:36 -0400

    Modifying background image for landing page, style changes to many sections

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatCarousel, MatCarouselComponent } from '@ngmodule/material-carousel';
-
 
 @Component({
   selector: 'app-testimonials',
@@ -25,7 +23,10 @@ export class TestimonialsComponent implements OnInit {
       role: 'Lead Application Developer'
     },
     {
-      message: 'Placeholder text....',
+      message: 'Tyler is a dependable developer who is quick on his feet when it comes to researching and resolving issues. ' +
+               'He is my go-to developer to ask questions and is always willing to take the time to explain what I ' +
+               'don\'t fully understand. ' +
+               'His code is reliable and I can always rely on his thorough unit testing.',
       author: 'Anne-Marie Williams',
       role: 'Lead Business Analyst'
     }
